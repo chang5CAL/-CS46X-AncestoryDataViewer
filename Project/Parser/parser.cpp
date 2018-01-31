e07e58baf0497e5b606409c7689298d1b111612f
chang5cal <ginka13@gmail.com>
2018-01-31 03:13:06 -0800

    Added file io to parser and basic parsing. Need to add proper keywords and also make an iterator to run through the set.

--- Project/Parser/parser.cpp
+++ Project/Parser/parser.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <string>
 #include <unordered_set>
+#include <iostream>
+#include <fstream>
 //Really, this should all be in a class.
 
 using namespace std;
 
 
-unordered_set<node> nodes = ;
-unordered_set<families> families = ;
+unordered_set<node> nodes;
+unordered_set<families> families;
 
 struct node{
     //So we're just gonna treat the node as a name and
@@ -24,6 +26,7 @@ struct node{
 struct family{
     //So we're just gonna treat the node as a name and
     //their direct link. No siblings.
+    string id;
     string* children; //FAM
     string parent1; //FAMC
     string parent2; //FAMC
@@ -64,27 +67,106 @@ void linkNode(){
 
 int main()
 {
-    //Maybe a vector of pointers to keep track? Or a map?
-    //Hash set seems best, so we can have the ID of the person/family,
-    //So it points to them and so we can access them directly in O(1) time.
+    string line;
+    //This should actually be a constructor.
     node* n = NULL;
     families = new family[100]; //Temp, just set it to 100
-    while( != NULL){
-        //Read the file
-        if(){
-            //If it's a node
-            n = new node;
-            node->id = ;
-            nodes[/** id **/] = n;
-            //Keep going for a name...
-            while(WORD != NAME){
+
+    //This should go in a different function, like read or something. But then it should wipe it all.
+    ifstream file("temp.ged"); //Should allow manual input but this will suffice for now.
+    if(file.is_open()){
+        while(file >> line){
+                //Note to self: This should grab a word I think
+            //Read the file
+            if(line == "NODE"){
+                //If it's a node
+                //Keep going for a name...
+                n = new node;
+
+                bool nameFound = false;
+                bool idFound = false;
+
+                while(file >> line){
+                    if(line == "ID"){
+                        file >> line;
+                        n->id = line;
+
+                        idFound = true;
+                    }
+                    else if (line == "NAME"){
+                        file >> line;
+                        n->name = line;
+
+                        nameFound = true;
+                    }
+
+                    if(idFound && nameFound){
+                        break;
+                        //n has a name and id, so we're good!
+                    }
+                }
+                nodes[idTemp] = n;
+                n = NULL;
+            }
+            else if (line == "FAM"){
+                //If it's a family, make the family.
+                family* fTemp = new family;
+                fTemp->numChildren = 0;
+                bool idFound = false;
+                bool parent1Found = false;
+                bool parent2Found = false;
+                bool childFound = false;
+
+                while(file >> line && line != "WHATEVER_SIGNALS_FAMILY_DONE"){
+                    if(line == "ID"){
+                        //Grab the family's id.
+                        file >> line;
+                        fTemp->id = line;
+
+                        idFound = true;
+                    }
+                    else if (line == "CHILD"){
+                        //I think having at least one child is mandatory for a GEDCOM,
+                        //but I don't know. Parents ARE mandatory. Probably.
+                        file >> line;
+
+                        fTemp->children[fTemp->numChildren] = line;
+                        fTemp->numChildren++;
+
+                        childFound = true;
+                    }
+                    else if (line == "PARENT"){
+                        file >> line;
+
+                        if(!parent1Found){
+                            parent1Found = true;
+                            fTemp->parent1 = line;
+                        }
+                        else if (!parent2Found){
+                            parent2Found = true;
+                            fTemp->parent2 = line;
+                        }
+                        else{
+                            cout << "WHOOPS." << endl;
+                            //Throw an error you have three parents!
+                        }
+                    }
+                }
+                if(!parent1Found || !parent2Found || !childFound || !idFound){
+                    //You're missing something
+                    cout << "Whoops." << endl;
+                    //So it should error.
+                }
+                families[fTemp->id] = ;
 
             }
         }
-        else if (){
-            //If it's a family, make the family.
-
-        }
     }
+    else{
+        cout << "Error, file could not be opened." << endl;
+    }
+
+    //Now make an iterator and have it run through the families unordered set.
+
     return 0;
 }
