820efa8b0ad921b802e2760d8aa2e1b9ccb4a242
chang5cal <ginka13@gmail.com>
2018-03-06 17:45:52 -0800

    Added algorithm

--- Project/SortingAlgorithm/main.cpp
+++ Project/SortingAlgorithm/main.cpp
@@ -1,61 +1,94 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
-using namespace std;
 
-///Note to self; Draw it out, since you seem to not know how to draw positions
+std::vector<std::vector<double>> levelMaxVal;
+//This should be [tree][level]
 
 
 /*Temporary, remove later*/
 struct Person {
-	string id;
-	string name;
-	string sprouse;
-	string father;
-	string mother;
-	vector<string> children;
-};
-
-struct PosNode{
-    string id;
+	std::string id;
+	std::string name;
+	std::string sprouse;
+	std::string father;
+	std::string mother;
+	std::vector<std::string> children;
     double pos = 0;
     double range = 0;
     int level = 0; //This is the Y level. It goes up as the range
-    //finder recurses, so we can go back and set the range of others later.
 };
 
-vector<node> findRoots(unordered_map family){
-    vector<node> roots;
-    for(auto it = family.begin; it != family.end(); ++it){
-        if(it->second.parent1 == NULL && it->second.parent2 == NULL
-           && ((it->second.spouse.parent1 == NULL && it->second.spouse.parent2 == NULL)
-               || (it->second.spouse == NULL))){
+    std::vector<std::string> roots;
+    std::vector<std::string> findRoots(std::vector<Person> family){
+    //We just store the id.
+    bool spouseAdded = false;
+    for(int i=0;i<family.size();++i){
+        if(family[i].father == NULL && family[i].mother == NULL
+           && ((family[i].spouse.father == NULL && family[i].spouse.mother == NULL)
+               || (family[i].spouse == NULL))){
             //That's a hefty if, but it basically just checks if the node has parents,
             //and if their spouse either exists, or if their spouse also has no parents.
-            roots.push_back(it->second.id);
+            for(int j=0;j<roots.size();++j){
+                if(roots[j] == family[i].spouse){
+                    //Will need to come back to solve divorces.
+                    spouseAdded = true;
+                }
+            }
+            //This for makes sure the spouse hasn't shown up before.
+            if(!spouseAdded){
+                //If the spouse has been added already, don't add.
+                roots.push_back(family[i].id);
+            }
+
+            spouseAdded = false;
+            //So the fatal flaw is that we assume the roots married once and had children.
+            //If W-X and Y-Z both had children, then X-Y and W-Z got together, we'll forget to
+            //push one in.
+            //Fortunately, GEDCOM files only allow one spouse, so we just need to keep track of
+            //divorces.
         }
     }
     return roots;
 }
 
-//So I realized this can actually be done like the Fibonacci sequence.
-double spacingChildren(PosNode* b,Person p,int level){
+void levelInsert(int tree,int level, double val){
+    //Checks if an inputted value is greater than
+    //the current value, and if it doesn't exist,
+    //create all levels until it does.
+    while(levelMaxVal[tree].size() < level){
+        levelMaxVal[tree].push_back(0);
+    }
+    if(levelMaxVal[tree][level-1] < val){
+        levelMaxVal[tree][level-1] = val;
+    }
+    //Remember, if we get 3, we add 0,1,2 as 0,0,0. Therefore 2 is the third value.
+
+}
+
+//This is similar to the fibonacci sequence.
+double spacingChildren(int tree,std::string id,std::vector<Person> p,int level){
+    //This should give me the root for the id to start with.
     double maxVal = 0;
     double curVal = 0;
 
-    level++; //Increment level for each. Start level at -1 when running this.
-    b.level = level;
+    level++; //Increment level for each. Start level at 0.
+    p[id].level = level-1;
+    //This should be find id function, but I don't know how that one works.
 
-    if(p.children.size() == 0){
+    if(p[id].children.size() == 0){
         //If you have no children, your size is always 3.
-        b.range = 3;
+        p[id].range = 3;
+        levelInsert(tree,level,3);
         return 3;
         //Base case is 3 because it's one for the person, one for their link and one for their spouse.
         //O-O <- like this
     }
 
-    for(int i=0;i<;++i){
+    for(int i=0;i<p[id].children.size();++i){
         //Find the maximum child
-        curVal = spacingChildren(p.children[i]);
+        curVal = spacingChildren(tree,p[id].children[i],level);
         //We're going to recursively call this function, stopping when
         //a person has no children, in which case it goes in to the
         //above base case.
@@ -69,10 +102,10 @@ double spacingChildren(PosNode* b,Person p,int level){
         //    o
         // |--|--|
         // o  o  o
-        //|-|
-        //o o
+        //    | |-|
+        //    o o o
        //The spacing should be 18 for the parent, and 6 for the children EACH.
-       //This is because the leftmost child has two children, so the bottom-most's
+       //This is because the rightmost child has two children, so the bottom-most's
        //maxVal would return 3 each, and there's two children so below it would
        //set maxVal for the parent as 3*2 to 6.
         if(curVal > maxVal){
@@ -80,63 +113,49 @@ double spacingChildren(PosNode* b,Person p,int level){
         }
 
     }
-    for(int i=0;i<;++i){
-        //The above for loop got the largest value per child.
-        //Apply the maxVal to all direct children of the node.
-        b[p.children[i]].range = maxVal;
-    }
 
-    maxVal = maxVal * p.children.size();
+    maxVal = maxVal * p[id].children.size();
     //Now the max value is set to the amount of children * the largest node
 
-    b.range = maxVal;
+    p[id].range = maxVal;
+    levelInsert(tree,level,maxVal);
+
     return maxVal;
 }
 
-unordered_map<int,int> levelEqualize(unordered_map<string,PosNode> b,Person p,unordered_map<int,int> maxLevelPos){
-    //Start at the 0th level. That one has no siblings, or it wouldn't be a progenitor.
-    for(int i=0;i<p.children;++i){
-        //Since we start at the progenitor, this one will be 1.
-        if(maxLevelPos[b[p.children[i]].level] == NULL){
-            //If the level doesn't exist, fill it.
-            maxLevelPos[b[p.children[i]].level] = 0;
-        }
-        else if(b[p.id].pos > maxLevelPos[b[p.children[i]].level]){
-            //Check if the current max value is greater than the current max
-            maxLevelPos[b.level] = b.pos;
-        }
-        levelEqualize(b[p.children[i]],p[p.children[i]],maxLevelPos);
-        //So go down the tree and figure out the next level, and recurse back up. Need to go through
-        //all the values anyways.
-    }
-    return maxLevelPos;
-}
-
-void place(PosNode b,Person p,double minPos,unordered_map<int,int> maxLevelPos){
-    //Once ranges are done, place positions
-    //First place is half it's range, minus 1.
-    if(b.level == 0){
-        b.pos = b.range/2 - 1;
-    }
-    else{
-        b.pos = maxLevelPos[b.level]/p.children.size() - 1 + minPos;
-    }
+void place(int tree,std::string id,std::vector<Person> p,double minPos){
+    //The position is always half the range minus 1, to leave room for the
+    //connector pipe and the spouse.
+    p[id].pos = levelMaxVal[tree][p[id].level] - 1 + minPos;
+    //minPos starts at 0.
 
     //So we know that all of the children will have the same range.
-    for(int i=0;i<p[b.id].children.size();++i){
+    for(int i=0;i<p[id].children.size();++i){
         //So we'll do each subtree at a time.
-        place(b[p.children[i]],p.children[i],b.pos/p[b.id].children.size());
-        minPos += b.range/p[b.id].children.size();
-        //So run this function again with the new position, starting at 0,
-        //or whatever the min is/was. Then it places the node in the middle
-        //of it's block - 1.
-        //Rinse repeat.
+        place(tree,p[id].children[i],p,minPos);
+        minPos += levelMaxVal[tree][p[id].level+1]; //Increments by the child's max range.
+        //We want the range below the current node for the minPos, not the range of the
+        //current node. Never worry about the +1 because having a child is a requisite for
+        //this loop to run.
 
+        //Remember that the parent is in charge of placing it's children.
+
+    }
+}
+
+
+void startSort(){
+    //This is the only public function. It runs all the above functions.
+    //We assume the parsed data, f, is given and global.
+    std::vector<std::string> p = findRoots(f); //Where f is the parsed data
+    for(int i=0;i<p.size();++i){
+        spacingChildren(i,p[i],f,0);
+        place(i,p[i],f,0);
     }
 }
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    std::cout << "Hello world!" << std::endl;
     return 0;
 }
