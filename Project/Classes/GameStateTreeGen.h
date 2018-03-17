59f90e103d76165af94d10b8c084dcec08673115
chang5CAL <ginka13@gmail.com>
2018-03-17 14:18:50 -0700

    Added test gedcom, fixed more errors

--- Project/Classes/GameStateTreeGen.h
+++ Project/Classes/GameStateTreeGen.h
@@ -3,15 +3,15 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/GameStateBase.h"
+#include "GameFramework/GameModeBase.h"
 #include "Engine/World.h"
 #include "Engine/StaticMeshActor.h"
 #include "Components/TextRenderComponent.h"
 #include "LevelEditor.h"
 #include "Editor.h"
-#include "NodeBluePrintScript.h"
 #include "Engine.h"
 #include "Components/ShapeComponent.h"
+#include "NodeBluePrintScript.h"
 
 #include <iostream>
 #include <stdlib.h>
@@ -21,53 +21,59 @@
 #include <stdio.h>
 #include <algorithm>
 #include <vector>
-#include "GameStateTreeGen.generated.h"
 
+#include "GameStateTreeGen.generated.h"
 /**
-*
-*/
+ * 
+ */
 UCLASS()
-class ADVR_API AGameStateTreeGen : public AGameStateBase
+class ADVR_API AGameStateTreeGen : public AGameModeBase
 {
+
 	GENERATED_BODY()
 
 	TSubclassOf<class UNodeBluePrintScript> Node;
-public:
-	struct Person {
-		std::string id;
-		std::string name;
-		std::string spouse;
-		std::string father;
-		std::string mother;
-		std::vector<std::string> children;
-		float pos = 0;
-		float range = 0;
-		float level = 0; //This is the Y level. It goes up as the range
-		int treeLevel;
-		bool placed = false;
-	};
+	public:
+		struct Person {
+			std::string id;
+			std::string name;
+			std::string spouse;
+			std::string father;
+			std::string mother;
+			std::vector<std::string> children;
+			float pos = 0;
+			float range = 0;
+			float level = 0; //This is the Y level. It goes up as the range
+			int treeLevel;
+			bool placed = false;
+		};
 
-	AGameStateTreeGen();
-private:
-	std::vector<std::string> roots;
-	std::vector<std::vector<float>> levelMaxVal;
-	std::vector<Person> parsedData;
+		AGameStateTreeGen();
+	private:
+		std::vector<std::string> roots;
+		std::vector<std::vector<float>> levelMaxVal;
+		std::vector<Person> parsedData;
 
 
-	//Parser Functionality
-	int search_by_id(std::vector<Person> p, std::string str);
-	std::vector<Person> parse(std::string fileLocation);
+		//Parser Functionality
+		int search_by_id(std::vector<Person> p, std::string str);
+		std::vector<Person> parse(std::string fileLocation);
 
-	//Sorting algorithm functionality
-	void levelInsert(int tree, int level, float val);
-	std::vector<std::string> findRoots(std::vector<Person> family);
-	float spacingChildren(int tree, std::string sid, std::vector<Person> p, int level);
-	void place(int tree, std::string sid, std::vector<Person> p, float minPos);
+		//Sorting algorithm functionality
+		void levelInsert(int tree, int level, float val);
+		std::vector<std::string> findRoots(std::vector<Person> family);
+		float spacingChildren(int tree, std::string sid, std::vector<Person> p, int level);
+		void place(int tree, std::string sid, std::vector<Person> p, float minPos);
 
-	//Generate
-	void placeNodes(Person p, std::vector<Person> family, Person parent);
-	void placeSpouse(Person p, std::vector<Person> family);
+		//Generate
+		void placeNodes(Person p, std::vector<Person> family, Person parent);
+		void placeSpouse(Person p, std::vector<Person> family);
+	
 };
+
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
 
 
 
